b223209e2f8e36ff74439a3dc32de4bfa5a92561
Emad Aghayi <emad.aghayi@gmail.com>
2018-10-08 20:10:56 -0400

    delete funciton is implemented

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,37 +1,40 @@
 import React, {Component} from 'react';
 import {Table, Row, Col, Button} from 'reactstrap';
 import axios from 'axios';
+
 const JsonTable = require('ts-react-json-table');
 
 class TodoList extends Component {
     state = {
         todoArray: []
     };
-    deleteIt =(event)=>{
+
+    deleteIt = (event) => {
 
         const todo = {
             id: event.target.value
         };
-        axios.post('/endpoints/deleteTodo',{todo})
-            .then(res => {
-                    if (res.status === 200) {
-                        this.setState({
-                            todoTitle: '',
-                            todoDescription: '',
-                            todoPriority: 1
-                        });
+        // new Promise((resolve, reject) => {
+        console.log('ex1');
+        const resultOfDelete = axios.post('/endpoints/deleteTodo', {todo});
 
-                        alert("You have saved a todo Successfully!");
+        console.log('resultOfDelete', resultOfDelete);
+        console.log('ex2');
+        resultOfDelete.then(response => {
+            console.log('response',response);
 
-                    } else {
-                        alert("Error:" + res);
-                    }
-                    console.log('addTodo response: ', res);
-                    // console.log(res.data);
-                }
-            )
+            if (response.status === 200) {
+                alert("You have DELETED a todo Successfully!");
+                console.log('ex4');
+            }else{
+                alert("Error, failed to DELETE!");
+            }
+        }).catch(error => {
+            alert('! ' + error);
+        });
 
-    };
+    }
+
 
     fetchData() {
         if (this.props.data.current == null) {
@@ -41,7 +44,7 @@ class TodoList extends Component {
         console.log('list2:', todoListJson);
         var result = [];
         for (var key in todoListJson.todoList) {
-            //console.log('keys:', key);
+
             result.push(todoListJson.todoList[key]);
         }
 
@@ -61,7 +64,7 @@ class TodoList extends Component {
         console.log('state', this.state.todoArray);
 
         return result2;
-        // return this.state.todoArray;
+
     }
 
     render() {
@@ -84,13 +87,13 @@ class TodoList extends Component {
                             {this.fetchData().map(item => {
                                 return (
                                     <tr key={item.id}>
-                                        {/*<th scope="row">1</th>*/}
                                         <td>{item.title}</td>
                                         <td>{item.description}</td>
                                         <td>{item.status}</td>
 
                                         <td>
-                                            <Button color="secondary" size="sm" block onClick={this.deleteIt} value={item.id}>Delete it</Button>
+                                            <Button color="secondary" size="sm" block onClick={this.deleteIt}
+                                                    value={item.id}>Delete it</Button>
 
                                             <Button color="secondary" size="sm" block>Update it</Button>
                                         </td>
@@ -113,139 +116,139 @@ class TodoList extends Component {
         );
     }
 
-    // fetch('/endpoints/fetchAllTodos',{"emadaghayi"})
-    //         <div>{JSON.stringify(this.fetchData())}</div>
-    //     .then(res => res.json())
-    //     .then(users => this.setState({users}));
+// fetch('/endpoints/fetchAllTodos',{"emadaghayi"})
+//         <div>{JSON.stringify(this.fetchData())}</div>
+//     .then(res => res.json())
+//     .then(users => this.setState({users}));
 //{/*<div>{this.fetchData().map(function(object) {
 //                 return (
 //                     <JsonTable rows={object} />
 //                 );
 //             })}</div>*/}
 
-    // async fetchListOfTodo() {
-    //     var result = await fetch('/endpoints/fetchAllTodos', {
-    //         method: 'POST',
-    //         headers: {
-    //             Accept: 'application/json',
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify({
-    //             userId: 'emadaghayi'
-    //         }),
-    //     });
-    //     const todoListJson = await result.json();
-    //
-    //     // for(var key in dlist){
-    //     //     var keyjson = dlist[key];
-    //     //     console.log(keyjson)
-    //     // }
-    //     for (var key in todoListJson) {
-    //         console.log("Key: " + key);
-    //         console.log("Value: " + todoListJson[key].id);
-    //         this.state.todoArray.push({
-    //             "id": todoListJson[key].id,
-    //             "title": todoListJson[key].title,
-    //             "description": todoListJson[key].description,
-    //             "status": todoListJson[key].status,
-    //             "priority": todoListJson[key].priority,
-    //             "address": todoListJson[key].address,
-    //             "repeat": todoListJson[key].repeat
-    //         });
-    //     }
-    //
-    //     // this.setState({todoArray: todoList});
-    //     console.log('in Fetch:', this.state.todoArray);
-    //
-    //
-    // }
+// async fetchListOfTodo() {
+//     var result = await fetch('/endpoints/fetchAllTodos', {
+//         method: 'POST',
+//         headers: {
+//             Accept: 'application/json',
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             userId: 'emadaghayi'
+//         }),
+//     });
+//     const todoListJson = await result.json();
+//
+//     // for(var key in dlist){
+//     //     var keyjson = dlist[key];
+//     //     console.log(keyjson)
+//     // }
+//     for (var key in todoListJson) {
+//         console.log("Key: " + key);
+//         console.log("Value: " + todoListJson[key].id);
+//         this.state.todoArray.push({
+//             "id": todoListJson[key].id,
+//             "title": todoListJson[key].title,
+//             "description": todoListJson[key].description,
+//             "status": todoListJson[key].status,
+//             "priority": todoListJson[key].priority,
+//             "address": todoListJson[key].address,
+//             "repeat": todoListJson[key].repeat
+//         });
+//     }
+//
+//     // this.setState({todoArray: todoList});
+//     console.log('in Fetch:', this.state.todoArray);
+//
+//
+// }
 
-    // componentDidMount() {
-    //
-    //     this.fetchListOfTodo();
-    //
-    //     console.log('mount:', this.state.todoArray)
-    // }
+// componentDidMount() {
+//
+//     this.fetchListOfTodo();
+//
+//     console.log('mount:', this.state.todoArray)
+// }
 
 
-    // render() {
-    //     const items = [
-    //         {"id": 75950, "name": "Louella Wallace", "age": 24, "phone": "+44 (0)203 437 7302", "color": "green"},
-    //         {"id": 80616, "name": "Hanson Perry", "age": 36, "phone": "+44 (0)203 279 3708", "color": "brown"},
-    //         {"id": 77621, "name": "Brandi Long", "age": 20, "phone": "+44 (0)203 319 4880", "color": "gray"},
-    //     ];
-    //
-    //     if (this.state.todoArray === []) {
-    //         console.log('adfa');
-    //         this.setState({todoArray: items});
-    //
-    //     } else {
-    //         console.log('after')
-    //     }
-    //     ;
-    //     return (
-    //         <div>
-    //             <div>
-    //                 {/*<JsonTable rows={items}/>*/}
-    //
-    //                 < JsonTable rows={this.state.todoArray}/>
-    //
-    //             </div>
-    //             {/*<div className="container">*/}
-    //                 {/*<br/>*/}
-    //                 {/*<br/>*/}
-    //                 {/*<h3>List of todos</h3>*/}
-    //                 {/*<Table responsive>*/}
-    //                     {/*<thead>*/}
-    //                     {/*<tr>*/}
-    //                         {/*<th>#</th>*/}
-    //                         {/*<th>Title</th>*/}
-    //                         {/*<th>Description</th>*/}
-    //                         {/*<th>Priority</th>*/}
-    //                         {/*<th>Duo date</th>*/}
-    //                         {/*<th>Created date</th>*/}
-    //                         {/*<th>Repeat</th>*/}
-    //                         {/*<th>Operation</th>*/}
-    //                     {/*</tr>*/}
-    //                     {/*</thead>*/}
-    //                     {/*<tbody>*/}
-    //                     {/*<tr>*/}
-    //                         {/*<th scope="row">1</th>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                     {/*</tr>*/}
-    //                     {/*<tr>*/}
-    //                         {/*<th scope="row">2</th>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                     {/*</tr>*/}
-    //                     {/*<tr>*/}
-    //                         {/*<th scope="row">3</th>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                         {/*<td>Table cell</td>*/}
-    //                     {/*</tr>*/}
-    //                     {/*</tbody>*/}
-    //                 {/*</Table>*/}
-    //             {/*</div>*/}
-    //         </div>
-    //
-    //     );
-    // }
+// render() {
+//     const items = [
+//         {"id": 75950, "name": "Louella Wallace", "age": 24, "phone": "+44 (0)203 437 7302", "color": "green"},
+//         {"id": 80616, "name": "Hanson Perry", "age": 36, "phone": "+44 (0)203 279 3708", "color": "brown"},
+//         {"id": 77621, "name": "Brandi Long", "age": 20, "phone": "+44 (0)203 319 4880", "color": "gray"},
+//     ];
+//
+//     if (this.state.todoArray === []) {
+//         console.log('adfa');
+//         this.setState({todoArray: items});
+//
+//     } else {
+//         console.log('after')
+//     }
+//     ;
+//     return (
+//         <div>
+//             <div>
+//                 {/*<JsonTable rows={items}/>*/}
+//
+//                 < JsonTable rows={this.state.todoArray}/>
+//
+//             </div>
+//             {/*<div className="container">*/}
+//                 {/*<br/>*/}
+//                 {/*<br/>*/}
+//                 {/*<h3>List of todos</h3>*/}
+//                 {/*<Table responsive>*/}
+//                     {/*<thead>*/}
+//                     {/*<tr>*/}
+//                         {/*<th>#</th>*/}
+//                         {/*<th>Title</th>*/}
+//                         {/*<th>Description</th>*/}
+//                         {/*<th>Priority</th>*/}
+//                         {/*<th>Duo date</th>*/}
+//                         {/*<th>Created date</th>*/}
+//                         {/*<th>Repeat</th>*/}
+//                         {/*<th>Operation</th>*/}
+//                     {/*</tr>*/}
+//                     {/*</thead>*/}
+//                     {/*<tbody>*/}
+//                     {/*<tr>*/}
+//                         {/*<th scope="row">1</th>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                     {/*</tr>*/}
+//                     {/*<tr>*/}
+//                         {/*<th scope="row">2</th>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                     {/*</tr>*/}
+//                     {/*<tr>*/}
+//                         {/*<th scope="row">3</th>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                         {/*<td>Table cell</td>*/}
+//                     {/*</tr>*/}
+//                     {/*</tbody>*/}
+//                 {/*</Table>*/}
+//             {/*</div>*/}
+//         </div>
+//
+//     );
+// }
 
 
 }
